413bcefc9bdcafcc9d42b543e5ce38465adf409f
Alex <sanasol2008@yandex.ru>
2020-04-13 21:43:03 +0300

    Update Middleware.php

--- src/DataObjects/Middleware.php
+++ src/DataObjects/Middleware.php
@@ -9,9 +9,11 @@ class Middleware
 
     public function __construct(string $middleware)
     {
-        $tokens = explode(':', $middleware, 2);
-        $this->name = $tokens[0];
-        $this->parameters = isset($tokens[1]) ? explode(',', $tokens[1]) : [];
+        if(is_string($middleware)) {
+            $tokens = explode(':', $middleware, 2);
+            $this->name = $tokens[0];
+            $this->parameters = isset($tokens[1]) ? explode(',', $tokens[1]) : [];
+        }
     }
 
     public function name()
